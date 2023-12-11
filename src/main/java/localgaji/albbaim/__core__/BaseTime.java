5511c03fe588374cb176b3126c2f858bf21a0b89
localgaji <oorongcha@naver.com>
2023-12-11 23:44:23 +0900

    refactor: createdAt 자동 등록

---
+++ src/main/java/localgaji/albbaim/__core__/BaseTime.java
@@ -0,0 +1,22 @@
+package localgaji.albbaim.__core__;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
