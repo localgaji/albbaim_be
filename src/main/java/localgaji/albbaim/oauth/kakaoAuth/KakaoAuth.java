917aa510ab3249df1dcda58d0872fafaeb38927e
localgaji <oorongcha@naver.com>
2023-12-11 14:19:17 +0900

    feat: 소셜 정보-카카오 도메인 생성

---
+++ src/main/java/localgaji/albbaim/oauth/kakaoAuth/KakaoAuth.java
@@ -0,0 +1,25 @@
+package localgaji.albbaim.oauth.kakaoAuth;
+
+import localgaji.albbaim.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@Entity @Table(name = "kakao_auth")
+@Getter @Builder @NoArgsConstructor @AllArgsConstructor
+public class KakaoAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column @NotNull
+    private Long kakaoId;
+
+}
