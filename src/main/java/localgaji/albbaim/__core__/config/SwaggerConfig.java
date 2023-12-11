f6e075c8ca795bb7ae5dee8fcc83f9f783c76192
localgaji <oorongcha@naver.com>
2023-12-11 13:46:08 +0900

    chore: config 생성

---
+++ src/main/java/localgaji/albbaim/__core__/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package localgaji.albbaim.__core__.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Albbaim API")
+                        .description("스케줄 알빠임 API 명세서")
+                        .version("v1.0.0"));
+    }
+}
