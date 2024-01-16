e23a11ddebc8a953bc21377996253325da6bbcc8
localgaji <oorongcha@naver.com>
2024-01-16 18:59:44 +0900

    feat: 초대장 엔티티 생성

---
+++ src/main/java/localgaji/albbaim/workplace/invitation/Invitation.java
@@ -0,0 +1,44 @@
+package localgaji.albbaim.workplace.invitation;
+
+import localgaji.albbaim.__core__.BaseTime;
+import localgaji.albbaim.workplace.Workplace;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Builder @AllArgsConstructor @NoArgsConstructor
+@Table(name = "invitation") @DynamicInsert
+public class Invitation extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long invitationId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Workplace workplace;
+
+    @Column(unique = true)
+    private String invitationKey;
+
+    @Column
+    private LocalDateTime keyUpdatedDate;
+
+    @Column @Builder.Default
+    private int durationHours = 720;
+
+    public void updateInvitation(String invitationKey, LocalDateTime keyUpdatedDate) {
+        this.invitationKey = invitationKey;
+        this.keyUpdatedDate = keyUpdatedDate;
+    }
+
+    public void updateDurationHours(int durationHours) {
+        this.durationHours = durationHours;
+    }
+
+    public boolean isExpired() {
+        return keyUpdatedDate.plusHours(durationHours).isAfter(LocalDateTime.now());
+    }
+}
