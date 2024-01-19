7b98d3d23f0ccf9b29b0d94ee4c3475a1dd18d20
localgaji <oorongcha@naver.com>
2024-01-19 22:45:14 +0900

    test: ResultActions 요청 util

---
+++ src/test/java/localgaji/albbaim/utils/JsonRequest.java
@@ -0,0 +1,20 @@
+package localgaji.albbaim.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@AllArgsConstructor
+public class JsonRequest {
+    MockMvc mockMvc;
+
+    public <T> ResultActions sendPostRequest(String url, T request) throws Exception {
+        return mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(request)));
+    }
+}
