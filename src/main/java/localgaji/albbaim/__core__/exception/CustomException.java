1454e989871f5c586e9c0a8013e08cc1968e45b7
localgaji <oorongcha@naver.com>
2024-01-23 00:49:30 +0900

    refactor: 커스텀 에러 메세지 추가

--- src/main/java/localgaji/albbaim/__core__/exception/CustomException.java
+++ src/main/java/localgaji/albbaim/__core__/exception/CustomException.java
@@ -3,7 +3,13 @@ package localgaji.albbaim.__core__.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter @AllArgsConstructor
+@Getter
 public class CustomException extends RuntimeException {
     public ErrorType errorType;
+    public String message;
+
+    public CustomException(ErrorType errorType) {
+        this.errorType = errorType;
+        this.message = errorType.getErrorMessage();
+    }
 }
--- src/main/java/localgaji/albbaim/__core__/exception/ErrorType.java
+++ src/main/java/localgaji/albbaim/__core__/exception/ErrorType.java
@@ -5,22 +5,24 @@ import lombok.Getter;
 @Getter
 public enum ErrorType {
 
-    ETC_ERROR(400, -10001),
-    KAKAO_AUTH_FAIL(400, -10001),
-    INVALID_TOKEN(400, -21000),
-    EXPIRED_TOKEN(400, -21000),
-    GROUP_NOT_FOUND(404, -10001),
-    MEMBER_NOT_FOUND(404, -10006),
-    INVALID_INVITATION(404, -20004),
-    GROUP_ALREADY(400, -10001),
-    DAILY_NOT_FOUND(404, -11001);
+    ETC_ERROR(400, -10001, "기타 에러"),
+    KAKAO_AUTH_FAIL(400, -10001, "카카오 인증 실패"),
+    INVALID_TOKEN(400, -21000, "유효하지 않은 토큰"),
+    EXPIRED_TOKEN(400, -21000, "만료된 토큰"),
+    GROUP_NOT_FOUND(404, -10001, "매장 정보 없음"),
+    MEMBER_NOT_FOUND(404, -10006, "유저 정보 없음"),
+    INVALID_INVITATION(404, -20004, "유효하지 않은 초대장"),
+    GROUP_ALREADY(400, -10001, "매장 이미 있음"),
+    DAILY_NOT_FOUND(404, -11001, "");
 
 
     private final int statusCode;
     private final int internalCode;
+    private final String errorMessage;
 
-    ErrorType(int statusCode, int internalCode) {
+    ErrorType(int statusCode, int internalCode, String errorMessage) {
         this.statusCode = statusCode;
         this.internalCode = internalCode;
+        this.errorMessage = errorMessage;
     }
 }
