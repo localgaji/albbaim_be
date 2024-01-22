e21fa0ed1215f100d39fcd1709c57a0e54a2db92
localgaji <oorongcha@naver.com>
2024-01-22 23:36:19 +0900

    test: sample invitation 추가

--- src/test/java/localgaji/albbaim/utils/Samples.java
+++ src/test/java/localgaji/albbaim/utils/Samples.java
@@ -4,6 +4,9 @@ import localgaji.albbaim.auth.oauth.kakaoAuth.KakaoAuth;
 import localgaji.albbaim.auth.oauth.kakaoAuth.kakaoIdCache.KakaoIdCache;
 import localgaji.albbaim.auth.user.User;
 import localgaji.albbaim.workplace.Workplace;
+import localgaji.albbaim.workplace.invitation.Invitation;
+
+import java.time.LocalDateTime;
 
 public class Samples {
     public static User someUser() {
@@ -12,10 +15,20 @@ public class Samples {
                 .isAdmin(true)
                 .build();
     }
-
     public static Workplace someWorkplace() {
         return Workplace.builder()
-                .marketName("라이언월드")
+                .marketName("라이언 월드")
+                .marketNumber("1111111111")
+                .mainAddress("서울시 성동구 성수대로")
+                .detailAddress("1번지")
+                .build();
+    }
+
+    public static Invitation someInvitation(Workplace workplace) {
+        return Invitation.builder()
+                .invitationKey("abc")
+                .workplace(workplace)
+                .keyUpdatedDate(LocalDateTime.now())
                 .build();
     }
 
