9d0d087a1b9619e3782af49e85ab9dbef4267ecd
localgaji <oorongcha@naver.com>
2024-01-23 00:45:43 +0900

    test: invitation 테스트

---
+++ src/test/java/localgaji/albbaim/workplace/invitation/InvitationTest.java
@@ -0,0 +1,42 @@
+package localgaji.albbaim.workplace.invitation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static localgaji.albbaim.utils.Samples.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InvitationTest {
+
+    @DisplayName("초대장 만료 (안됨)")
+    @Test
+    void isNotExpired() {
+        // given
+        Invitation invitation = someInvitation(someWorkplace());
+        invitation.updateInvitation("123", LocalDateTime.now().minusHours(9));
+        invitation.updateDurationHours(10);
+
+        // when
+        boolean isExpired = invitation.isExpired();
+
+        // then
+        assertThat(isExpired).isFalse();
+    }
+
+    @DisplayName("초대장 만료 (됨)")
+    @Test
+    void isExpired() {
+        // given
+        Invitation invitation = someInvitation(someWorkplace());
+        invitation.updateInvitation("123", LocalDateTime.now().minusHours(11));
+        invitation.updateDurationHours(10);
+
+        // when
+        boolean isExpired = invitation.isExpired();
+
+        // then
+        assertThat(isExpired).isTrue();
+    }
+}
