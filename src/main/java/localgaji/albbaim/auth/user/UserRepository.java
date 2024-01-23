6d77ea9b8ed68a0fc3aede84199c2ea807832cd8
localgaji <oorongcha@naver.com>
2024-01-23 21:51:16 +0900

    refactor: unused method 제거

--- src/main/java/localgaji/albbaim/auth/user/UserRepository.java
+++ src/main/java/localgaji/albbaim/auth/user/UserRepository.java
@@ -1,13 +1,8 @@
 package localgaji.albbaim.auth.user;
 
-import localgaji.albbaim.workplace.Workplace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<List<User>> findByWorkplace(Workplace workplace);
 }
