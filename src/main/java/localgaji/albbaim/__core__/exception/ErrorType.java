d6a80f1a70985d84e0cb3e8117e8c6cc66367ed3
localgaji <oorongcha@naver.com>
2024-01-25 01:14:20 +0900

    feat: error type 수정

--- src/main/java/localgaji/albbaim/__core__/exception/ErrorType.java
+++ src/main/java/localgaji/albbaim/__core__/exception/ErrorType.java
@@ -6,15 +6,21 @@ import lombok.Getter;
 public enum ErrorType {
 
     ETC_ERROR(400, -10001, "기타 에러"),
-    KAKAO_AUTH_FAIL(400, -10001, "카카오 인증 실패"),
-    INVALID_TOKEN(400, -21000, "유효하지 않은 토큰"),
-    EXPIRED_TOKEN(400, -21000, "만료된 토큰"),
+
+    FORBIDDEN(403, -10001, "권한 없음"),
+
+    OAUTH_FAIL(401, -10001, "oauth 인증 실패"),
+    INVALID_TOKEN(401, -21000, "유효하지 않은 토큰"),
+    EXPIRED_TOKEN(401, -21000, "만료된 토큰"),
+
+    INVALID_INVITATION(404, -20004, "유효하지 않은 초대장"),
+
     GROUP_NOT_FOUND(404, -10001, "매장 정보 없음"),
     MEMBER_NOT_FOUND(404, -10006, "유저 정보 없음"),
-    INVALID_INVITATION(404, -20004, "유효하지 않은 초대장"),
-    GROUP_ALREADY(400, -10001, "매장 이미 있음"),
-    DAILY_NOT_FOUND(404, -11001, "");
+    NOT_FOUND(404, -11001, "없음"),
 
+    ALREADY_HAVE(400, -20001, "이미 있음"),
+    ALREADY_CLOSED(400, -20003, "이미 마감");
 
     private final int statusCode;
     private final int internalCode;
