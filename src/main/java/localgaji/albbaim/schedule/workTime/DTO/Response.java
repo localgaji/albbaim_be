5a5969f37f25a07073f73b3b0ba1a81c123f7640
localgaji <oorongcha@naver.com>
2024-01-24 22:57:45 +0900

    feat: WorkTime api dto 생성

---
+++ src/main/java/localgaji/albbaim/schedule/workTime/DTO/Response.java
@@ -0,0 +1,13 @@
+package localgaji.albbaim.schedule.workTime.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public class Response {
+    @Schema(description = "매니저 스케줄 모집 시작 : 스케줄 템플릿 조회")
+    public record GetTemplateResponse(
+            List<List<WorkTimeDTO>> template
+    ) {
+    }
+}
