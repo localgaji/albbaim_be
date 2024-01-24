e0cef634f534f2f750623fc1b024d201bf61b9ac
localgaji <oorongcha@naver.com>
2024-01-24 22:53:35 +0900

    feat: WorkTime 엔티티 생성

---
+++ src/main/java/localgaji/albbaim/schedule/workTime/WorkTime.java
@@ -0,0 +1,44 @@
+package localgaji.albbaim.schedule.workTime;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.persistence.*;
+import localgaji.albbaim.schedule.date.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "workTime") @Hidden
+@Getter @Builder @AllArgsConstructor @NoArgsConstructor
+public class WorkTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long workTimeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "date_id")
+    private Date date;
+
+    @Column
+    private String workTimeName;
+
+    @Column
+    private String startTime;
+
+    @Column
+    private String endTime;
+
+    @Column
+    private int headcount;
+
+    public void updateTime(String startTime, String endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    public void addWorkTimeToDate() {
+        this.date.getWorkTimeList().add(this);
+    }
+    public void updateHeadcount(int headcount) {
+        this.headcount = headcount;
+    }
+}
---
+++ src/main/java/localgaji/albbaim/schedule/workTime/WorkTimeRepository.java
@@ -0,0 +1,8 @@
+package localgaji.albbaim.schedule.workTime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkTimeRepository  extends JpaRepository<WorkTime, Long> {
+}
