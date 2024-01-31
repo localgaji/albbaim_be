d5e94aba33d0c7aa4223cdb84864def8c84454f7
localgaji <oorongcha@naver.com>
2024-01-31 22:57:17 +0900

    refactor: workplace service test

--- src/test/java/localgaji/albbaim/workplace/WorkplaceServiceTest.java
+++ src/test/java/localgaji/albbaim/workplace/WorkplaceServiceTest.java
@@ -3,6 +3,7 @@ package localgaji.albbaim.workplace;
 import localgaji.albbaim.user.User;
 import localgaji.albbaim.workplace.invitation.Invitation;
 import localgaji.albbaim.workplace.invitation.InvitationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,9 +12,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static localgaji.albbaim.__utils__.Samples.*;
+import static localgaji.albbaim.workplace.workplaceDTO.RequestWorkplace.*;
 import static localgaji.albbaim.workplace.workplaceDTO.ResponseWorkplace.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,22 +25,44 @@ class WorkplaceServiceTest {
     private WorkplaceService workplaceService;
     @Mock
     private InvitationService invitationService;
+    @Mock
+    private WorkplaceRepository workplaceRepository;
+    private final User user = someUser();
+    private final Workplace workplace = someWorkplace();
+
+    @DisplayName("매장 생성 (가입된 매장 없을 때)")
+    @Test
+    void addNewWorkplace() {
+        // given
+        PostAddGroupRequest request = PostAddGroupRequest.builder()
+                .workplaceName(workplace.getWorkplaceName())
+                .workplaceNumber(workplace.getWorkplaceNumber())
+                .mainAddress(workplace.getMainAddress())
+                .detailAddress(workplace.getDetailAddress())
+                .build();
+
+        // when
+        workplaceService.addNewWorkplace(user, request);
+
+        // then
+        verify(workplaceRepository).save(any(Workplace.class));
+        assertThat(user.getWorkplace()).isNotNull();
+    }
 
     @DisplayName("내 매장 정보 조회")
     @Test
     void findGroupInfo() {
         // given
-        User user1 = someUser();
-        User user2 = someUser();
+        User anotherUser = someUser();
         Workplace workplace = someWorkplace();
-        user1.updateWorkplace(workplace);
-        user2.updateWorkplace(workplace);
+        user.updateWorkplace(workplace);
+        anotherUser.updateWorkplace(workplace);
 
         // when
-        GetMyWorkplaceResponse response = workplaceService.findGroupInfo(user1);
+        GetMyWorkplaceResponse response = workplaceService.findGroupInfo(user);
 
         // then
-        assertThat(response.groupName()).isEqualTo(workplace.getMarketName());
+        assertThat(response.workplaceName()).isEqualTo(workplace.getWorkplaceName());
         assertThat(response.members().size()).isEqualTo(2);
     }
 
@@ -53,7 +78,7 @@ class WorkplaceServiceTest {
         GetInvitationInfoResponse response = workplaceService.findWorkplaceByInvitationKey(invitation.getInvitationKey());
 
         // then
-        assertThat(response.getMarketName()).isEqualTo(workplace.getMarketName());
+        assertThat(response.getWorkplaceName()).isEqualTo(workplace.getWorkplaceName());
     }
 
     @DisplayName("(매니저) 초대 키 발급")
@@ -62,7 +87,6 @@ class WorkplaceServiceTest {
         // given
         Workplace workplace = someWorkplace();
         Invitation invitation = someInvitation(workplace);
-        User user = someUser();
         user.updateWorkplace(workplace);
 
         when(invitationService.issueMyWorkplaceInvitation(any(Workplace.class)))
