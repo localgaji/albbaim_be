6b567ef89b64b17d5e8ac8c06402521d790d85e9
localgaji <oorongcha@naver.com>
2024-02-01 02:34:34 +0900

    feat: workTime -> application 조회 필드 추가

--- src/main/java/localgaji/albbaim/schedule/workTime/WorkTime.java
+++ src/main/java/localgaji/albbaim/schedule/workTime/WorkTime.java
@@ -2,6 +2,8 @@ package localgaji.albbaim.schedule.workTime;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import localgaji.albbaim.schedule.application.Application;
 import localgaji.albbaim.schedule.date.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,6 +11,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity @Table(name = "workTime") @Hidden
 @Getter @Builder @AllArgsConstructor @NoArgsConstructor
@@ -29,6 +33,10 @@ public class WorkTime {
 
     @Column
     private LocalTime endTime;
+
+    @OneToMany(mappedBy = "workTime")
+    @Builder.Default @NotNull
+    private List<Application> applicationList = new ArrayList<>();
 
     @Column
     private int headcount;
