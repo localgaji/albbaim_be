501aebd6aeef19991587a0b1e9c32b6f78ab0363
localgaji <oorongcha@naver.com>
2024-02-04 02:55:35 +0900

    refactor: 생성자 추가

--- src/main/java/localgaji/albbaim/schedule/__commonDTO__/WorkTimeWorkerListDTO.java
+++ src/main/java/localgaji/albbaim/schedule/__commonDTO__/WorkTimeWorkerListDTO.java
@@ -1,14 +1,21 @@
 package localgaji.albbaim.schedule.__commonDTO__;
 
+import localgaji.albbaim.user.User;
+import lombok.experimental.SuperBuilder;
+
 import java.util.List;
 
+@SuperBuilder
 public class WorkTimeWorkerListDTO extends WorkTimeDTO {
     List<Worker> workerList;
 
     public record Worker(
-            String userId,
+            Long userId,
             String userName
     ) {
+        public Worker(User user) {
+            this(user.getUserId(), user.getUserName());
+        }
     }
     public WorkTimeWorkerListDTO(String title, String startTime, String endTime, List<Worker> workerList) {
         super(title, startTime, endTime);
