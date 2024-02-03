fb5b03e816813f9da5e8a57290eda1d32cb68b60
localgaji <oorongcha@naver.com>
2024-02-04 02:56:05 +0900

    feat: application controller 서비스와 연결

--- src/main/java/localgaji/albbaim/schedule/application/ApplicationController.java
+++ src/main/java/localgaji/albbaim/schedule/application/ApplicationController.java
@@ -17,28 +17,36 @@ import static localgaji.albbaim.schedule.application.DTO.ApplicationResponse.*;
 @Tag(name = "스케줄 신청", description = "신청 정보 조회 / 수정")
 @RequestMapping("/application")
 public class ApplicationController {
+
+    private final ApplicationService applicationService;
+    private final ApplicationRecommendService applicationRecommendService;
+
     @GetMapping("/{startWeekDate}")
     @Operation(summary = "매니저 : 스케줄 신청자 명단 조회")
     public ResponseEntity<Response<GetApplyStatusResponse>> getApplyStatus(@AuthUser User user,
                                                                            @PathVariable String startWeekDate) {
-        return ResponseEntity.ok().build();
+        GetApplyStatusResponse response = applicationService.getApplicationList(user, startWeekDate);
+        return ResponseEntity.ok().body(success(response));
     }
     @GetMapping("/checklist/{startWeekDate}")
     @Operation(summary = "알바 스케줄 신청", description = "체크 리스트 조회")
     public ResponseEntity<Response<GetApplyFormResponse>> getApplyForm(@AuthUser User user,
                                                                        @PathVariable String startWeekDate) {
-        return ResponseEntity.ok().build();
+        GetApplyFormResponse response = applicationService.getApplicationCheckForm(user, startWeekDate);
+        return ResponseEntity.ok().body(success(response));
     }
     @PostMapping
     @Operation(summary = "알바 스케줄 신청", description = "신청 정보 저장")
     public ResponseEntity<Response<String>> postApply(@AuthUser User user,
                                                       @RequestBody PostApplyRequest requestBody) {
-        return ResponseEntity.ok().build();
+        applicationService.saveApplication(user, requestBody);
+        return ResponseEntity.ok().body(success(null));
     }
     @GetMapping("/recommend/{startWeekDate}")
     @Operation(summary = "매니저 스케줄 모집 마감", description = "추천 후보 조회")
     public ResponseEntity<Response<GetRecommendsResponse>> getRecommends(@AuthUser User user,
                                                                          @PathVariable String startWeekDate) {
-        return ResponseEntity.ok().build();
+        GetRecommendsResponse response = applicationRecommendService.getRecommends(user, startWeekDate);
+        return ResponseEntity.ok().body(success(response));
     }
 }
