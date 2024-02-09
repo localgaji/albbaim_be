d866265175ff17c8497207140dbcb192fd6cbb9c
localgaji <oorongcha@naver.com>
2024-02-10 03:51:21 +0900

    style: 띄어쓰기 2칸

--- src/main/java/localgaji/albbaim/schedule/workTime/WorkTimeRepository.java
+++ src/main/java/localgaji/albbaim/schedule/workTime/WorkTimeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkTimeRepository  extends JpaRepository<WorkTime, Long> {
+public interface WorkTimeRepository extends JpaRepository<WorkTime, Long> {
 }
