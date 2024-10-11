160f6e35291958d50eb3621b750a3bc8e10cb1b9
localgaji <oorongcha@naver.com>
2024-10-11 22:29:48 +0900

    fix: pessimistic lock 조회 method

--- src/main/java/localgaji/albbaim/schedule/replacement/ReplacementRepository.java
+++ src/main/java/localgaji/albbaim/schedule/replacement/ReplacementRepository.java
@@ -1,8 +1,17 @@
 package localgaji.albbaim.schedule.replacement;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ReplacementRepository extends JpaRepository<Replacement, Long> {
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select r from Replacement r where r.replacement_id = :replacement_id")
+    Optional<Replacement> findWithPessimisticLock(Long replacement_id);
+
 }
